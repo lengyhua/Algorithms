cfa921b8ca3fe347a9b1c79843d6325a0c047611
lialun <lial@kaola.com>
2014-05-20 19:25:19 +0800

    add prectise

--- src/main/java/com/bedpotato/Algorithms/C1/UnionFind.java
+++ src/main/java/com/bedpotato/Algorithms/C1/UnionFind.java
@@ -1,10 +1,30 @@
 package com.bedpotato.Algorithms.C1;
 
-import com.bedpotato.alg4.utils.StdIn;
+import com.bedpotato.alg4.constants.Alg4Constants;
+import com.bedpotato.alg4.utils.In;
 
-public class UnionFind {
-	private int id[];// 分量ID
-	private int count;// 分量数量
+public abstract class UnionFind {
+	int id[];// 分量ID
+	int count;// 分量数量
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		int[] numbers = In.readInts(Alg4Constants.DATA_PATH + "tinyUF.txt");
+		int count = numbers[0];
+		// QuickUnion
+		UnionFind_QuickUnion QU = new UnionFind_QuickUnion(count);
+		for (int i = 1; i < numbers.length; i += 2) {
+			QU.union(numbers[i], numbers[i + 1]);
+		}
+		System.out.println(QU.count());
+
+		// QuickFind
+		UnionFind_QuickFind QF = new UnionFind_QuickFind(count);
+		for (int i = 1; i < numbers.length; i += 2) {
+			QF.union(numbers[i], numbers[i + 1]);
+		}
+		System.out.println(QU.count());
+	}
 
 	public UnionFind(int number) {
 		count = number;
@@ -22,16 +42,7 @@ public class UnionFind {
 		return find(p) == find(q);
 	}
 
-	public int find(int p) {
-		// TODO
-		return 0;
-	}
+	public abstract int find(int p);
 
-	public void union(int p, int q) {
-
-	}
-
-	public static void main(String[] args) {
-		int N = StdIn.readInt();
-	}
+	public abstract void union(int p, int q);
 }
