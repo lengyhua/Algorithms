a08a54cdcdcdf9e8e492fc9d0f2f3e1e9798bd4c
lialun <lialun888@gmail.com>
2014-06-06 10:40:29 +0800

    add prectise

--- src/main/java/com/bedpotato/Algorithms/C2/SelectionSort.java
+++ src/main/java/com/bedpotato/Algorithms/C2/SelectionSort.java
@@ -18,11 +18,14 @@ public class SelectionSort extends SortBase {
 
 	@Override
 	public void practise(Comparable[] a) {
-		for(int i=0;i<a.length;i++){
-			int minNumberLocation = i;
-			for(int j=i;j<a.length;j++){
-				
+		for (int i = 0; i < a.length; i++) {
+			int min = i;
+			for (int j = i + 1; j < i; j++) {
+				if (less(a[j], a[min])) {
+					min = j;
+				}
 			}
+			exch(a, i, min);
 		}
 	}
 }
